1b31a79122658e1c589394381724260352f0f67b
RANGER-coder0517 <100456790+RANGER-coder0517@users.noreply.github.com>
2022-02-26 17:11:38 +0800

    Delete html directory

--- html/main.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    console.log('main');
-
-    var a = 20;
-    var b = 10;
-
-    window.x1 = a;
-    window.x2 = b;
-}) ();
--- html/main1.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    console.log('main1');
-
-    var a = "hello";
-    var b = "JS";
-
-    window.x1 = a;
-    window.x2 = b;
-}) ();
